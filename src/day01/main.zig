031dc72b5d7c5f2112f9d062d25ad83a7f0eda09
Cinition <cornee.n@gmail.com>
2024-12-02 17:01:52 +0100

    Day 1

--- src/day01/main.zig
+++ src/day01/main.zig
@@ -1,5 +1,41 @@
 const std = @import("std");
 
-pub fn execute() []const u8 {
-    return "Day 1";
+pub fn execute() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    const buffer = try allocator.alloc(u8, 20000);
+    defer allocator.free(buffer);
+
+    const content = try std.fs.cwd().readFile("./input.txt", buffer);
+
+    var leftList = std.ArrayList(u32).init(allocator);
+    var rightList = std.ArrayList(u32).init(allocator);
+    defer leftList.deinit();
+    defer rightList.deinit();
+
+    var lines = std.mem.tokenizeAny(u8, content, "\n");
+    while (lines.next()) |line| {
+        var numbers = std.mem.tokenizeAny(u8, line, " ");
+
+        const leftNumber = try std.fmt.parseInt(u32, numbers.next().?, 10);
+        const rightNumber = try std.fmt.parseInt(u32, numbers.next().?, 10);
+
+        try leftList.append(leftNumber);
+        try rightList.append(rightNumber);
+    }
+
+    std.mem.sort(u32, leftList.items, {}, std.sort.asc(u32));
+    std.mem.sort(u32, rightList.items, {}, std.sort.asc(u32));
+
+    var totalDistance: u32 = 0;
+
+    for (0..leftList.items.len) |i| {
+        const leftNumber: u32 = leftList.items[i];
+        const rightNumber: u32 = rightList.items[i];
+
+        totalDistance += @abs(leftNumber - rightNumber);
+    }
+
+    std.debug.print("Output: {}\n\n", .{totalDistance});
 }
--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,5 @@ pub fn main() !void {
     std.debug.print("Welcome to AOC 2024. Zig Edition!!!\n\n", .{});
 
     std.debug.print("Day 01\n", .{});
-    const day01_output = day01.execute();
-    std.debug.print("Output: {s}\n\n", .{day01_output});
+    try day01.execute();
 }
