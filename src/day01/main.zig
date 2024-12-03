f4a62e2ca1c53d71a2adb65d0dda13c84841d1ff
Cinition <cornee.n@gmail.com>
2024-12-03 01:52:00 +0100

    Problem 2 day 1

--- src/day01/main.zig
+++ src/day01/main.zig
@@ -1,20 +1,27 @@
 const std = @import("std");
 
+var allocator: std.mem.Allocator = undefined;
+
 pub fn execute() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
+    allocator = gpa.allocator();
 
     const buffer = try allocator.alloc(u8, 20000);
     defer allocator.free(buffer);
 
     const content = try std.fs.cwd().readFile("src/day01/input.txt", buffer);
 
+    std.debug.print("Problem 1 output: {d}\n\n", .{try problem1(content)});
+    std.debug.print("Problem 2 output: {d}\n\n", .{try problem2(content)});
+}
+
+fn problem1(data: []const u8) !i32 {
     var leftList = std.ArrayList(i32).init(allocator);
     var rightList = std.ArrayList(i32).init(allocator);
     defer leftList.deinit();
     defer rightList.deinit();
 
-    var lines = std.mem.tokenizeAny(u8, content, "\r\n");
+    var lines = std.mem.tokenizeAny(u8, data, "\r\n");
     while (lines.next()) |line| {
         var numbers = std.mem.tokenizeAny(u8, line, "   ");
         const leftInput = numbers.next().?;
@@ -30,14 +37,49 @@ pub fn execute() !void {
     std.mem.sort(i32, leftList.items, {}, std.sort.asc(i32));
     std.mem.sort(i32, rightList.items, {}, std.sort.asc(i32));
 
-    var totalDistance: u32 = 0;
+    var totalDistance: i32 = 0;
 
     for (0..leftList.items.len) |i| {
         const leftNumber: i32 = leftList.items[i];
         const rightNumber: i32 = rightList.items[i];
+        const value: i32 = @bitCast(@abs(leftNumber - rightNumber));
 
-        totalDistance += @abs(leftNumber - rightNumber);
+        totalDistance += value;
     }
 
-    std.debug.print("Output: {}\n\n", .{totalDistance});
+    return totalDistance;
+}
+
+fn problem2(data: []const u8) !i32 {
+    var leftList = std.ArrayList(i32).init(allocator);
+    var rightList = std.ArrayList(i32).init(allocator);
+    defer leftList.deinit();
+    defer rightList.deinit();
+
+    var lines = std.mem.tokenizeAny(u8, data, "\r\n");
+    while (lines.next()) |line| {
+        var numbers = std.mem.tokenizeAny(u8, line, "   ");
+        const leftInput = numbers.next().?;
+        const rightInput = numbers.next().?;
+
+        const leftNumber = try std.fmt.parseInt(i32, leftInput, 10);
+        const rightNumber = try std.fmt.parseInt(i32, rightInput, 10);
+
+        try leftList.append(leftNumber);
+        try rightList.append(rightNumber);
+    }
+
+    var totalDistance: i32 = 0;
+
+    for (0..leftList.items.len) |i| {
+        var num: i32 = 0;
+        for (rightList.items) |item| {
+            if (item == leftList.items[i]) {
+                num += 1;
+            }
+        }
+        totalDistance += leftList.items[i] * num;
+    }
+
+    return totalDistance;
 }
